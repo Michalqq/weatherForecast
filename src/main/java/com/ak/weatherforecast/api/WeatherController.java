39f422fe76c1478f48cc1be9e98b02d43b2ded71
michalqq <kraciukmichal@gmail.com>
2019-06-01 17:34:05 +0200

    refactored

--- src/main/java/com/ak/weatherforecast/api/WeatherController.java
+++ src/main/java/com/ak/weatherforecast/api/WeatherController.java
@@ -1,21 +1,22 @@
 package com.ak.weatherforecast.api;
 
-import com.ak.weatherforecast.model.WeatherModel;
+import com.ak.weatherforecast.service.OpenWeatherMapService;
+import com.ak.weatherforecast.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class WeatherController {
 
+    @Autowired
+    private WeatherService weatherService;
+
     @GetMapping("/weather")
     public String getWeather(@RequestParam String city, ModelMap modelMap) {
-        String apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city +
-                ",pl&appid=ef2028e98b54649bf1f4c4582631f350";
-        WeatherModel weatherModel = new RestTemplate().getForObject(apiUrl, WeatherModel.class);
-        modelMap.put("weather", weatherModel);
+        modelMap.put("weather", weatherService.getWeather(city));
         return "result";
     }
 }
